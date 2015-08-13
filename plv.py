382b57929d21513d6ad9480fa0d9d0cbc4f34e5f
Nevine Ebeid <nebeid@trustpointinnovation.com>
2015-08-13 15:03:38 -0400

    Added vectors for LA2 and lv(i,j) = plv1(i,j) XOR plv2(i,j)

--- plv.py
+++ plv.py
@@ -14,22 +14,6 @@ radix_8  = 2**8
 #Uncomment the following to obtain different values every time this script is run
 seed(333)
 
-print("LA1 ID (16 bits, padded to 32 bits) =")
-la_id1 = getrandbits(16)
-print(Hex(la_id1, radix_32))
-print()
-
-print("""
-i = 0
------
-j is randomly chosen in [1,20]
-""")
-
-print("j (in range [1,20], padded to 32 bits) =")
-j = randint(1,20)
-print(Hex(j, radix_32))
-print()
-
 
 def genls(id, i, la_id, ls_im1):
     if i == 0:
@@ -65,26 +49,62 @@ def genplv(id, i, la_id, ls_i, j):
     print("AES input plaintext (128 bits) = 0^{64-bit} || la_id" + str(id) + "^{32-bit} || j^{32-bit} = ")
     aes_in_j = "{0:032X}".format(la_id * radix_32 + j)
     print("0x" + aes_in_j)
-    cArrayDef("", "aes_in_j", long(aes_in_j, 16), 128/8, radix_8, False)
+    cArrayDef("", "aes_in"+ str(id) + "_j", long(aes_in_j, 16), 128/8, radix_8, False)
     print(os.linesep)
 
-    print("AES output (128 bits) = ")
+    print("AES output (128 bits) = AES_ls" + str(id) + "(" + str(i) +") (la_id" + str(id) + " || j)")
     aes_out_j = aes_obj.encrypt(aes_in_j.decode('hex')).encode('hex')
     print("0x" + aes_out_j.upper())
-    cArrayDef("", "aes_out_j", long(aes_out_j, 16), 128/8, radix_8, False)
+    cArrayDef("", "aes_out"+ str(id) + "_" + str(i) + "_j", long(aes_out_j, 16), 128/8, radix_8, False)
     print(os.linesep)
 
-    print("plv1(i,j) = AES output XOR AES input (128 bits) = ")
+    print("plv" + str(id) + "(" + str(i) + ",j) = AES output XOR AES input (128 bits) = ")
     plv_i_j = long(aes_in_j, 16) ^ long(aes_out_j, 16)
-    print(Hex(plv_i_j,radix_128))
-    cArrayDef("", "plv" + str(id) + "_" + str(i) +"_j", plv_i_j, 128/8, radix_8, False)
+    print(Hex(plv_i_j, radix_128))
+    cArrayDef("", "plv" + str(id) + "_" + str(i) + "_j", plv_i_j, 128/8, radix_8, False)
     print(os.linesep)
 
     return plv_i_j
 
+def genlv(i, plv1_i_j, plv2_i_j):
+    print("lv(" + str(i) + ",j) = plv1(" + str(i) + ",j) XOR plv2(" + str(i) + ",j)")
+    lv_i_j = plv1_i_j ^ plv2_i_j
+    print(Hex(lv_i_j, radix_128))
+    cArrayDef("", "lv_" + str(i) +"_j", lv_i_j, 128/8, radix_8, False)
+    print(os.linesep)
+
+    return lv_i_j
+    
+
+print("LA1 ID (16 bits, padded to 32 bits) =")
+la_id1 = getrandbits(16)
+print(Hex(la_id1, radix_32))
+print()
+
+print("LA2 ID (16 bits, padded to 32 bits) =")
+la_id2 = getrandbits(16)
+print(Hex(la_id2, radix_32))
+print()
+
+print("""
+i = 0
+-----
+j is randomly chosen in [1,20]
+""")
+
+print("j (in range [1,20], padded to 32 bits) =")
+j = randint(1,20)
+print(Hex(j, radix_32))
+print()
+
 ls1_0 = genls(1, 0, None, None)
 plv1_0_j = genplv(1, 0, la_id1, ls1_0, j)
-    
+
+ls2_0 = genls(2, 0, None, None)
+plv2_0_j = genplv(2, 0, la_id2, ls2_0, j)
+
+lv_0_j = genlv(0, plv1_0_j, plv2_0_j)
+
 print("""
 i = 1
 -----
@@ -98,3 +118,8 @@ print()
 
 ls1_1 = genls(1, 1, la_id1, ls1_0)
 plv1_1_j = genplv(1, 1, la_id1, ls1_1, j)
+
+ls2_1 = genls(2, 1, la_id2, ls2_0)
+plv2_1_j = genplv(2, 1, la_id2, ls2_1, j)
+
+lv_1_j = genlv(1, plv1_1_j, plv2_1_j)
