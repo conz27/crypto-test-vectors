af1f72026d7060bef748479140378b7d6f9e3959
Nevine Ebeid <nebeid@trustpointinnovation.com>
2015-10-02 11:25:02 -0400

    AES-CCM Decryption
    TODO: output test vectors

--- aesccm.py
+++ aesccm.py
@@ -28,24 +28,24 @@ def aes_ccm_enc(key, nonce, msg):
     ciphertext + tag
     '''
 
-    assert len(nonce) <= 12*2, "nonce must be of length less than or equal to 12 octets"
-    assert len(key) == 16*2, "key must be of length 16 octets"
     aes128_blk_len = 128/8
+    key_len = 128/8
     tag_len = 16
     nonce_len = 12
+    assert len(nonce) <= nonce_len*2, "nonce must be of length less than or equal to 12 octets"
+    assert len(key) == key_len*2, "key must be of length 16 octets"
     msg_len_len = 15 - nonce_len
-    msg_blk_len = (len(msg)/2)/aes128_blk_len    # not counting the last non-full block, if any
+    msg_blk_len = (len(msg)/2) / aes128_blk_len    # not counting the last non-full block, if any
     last_bytes = (len(msg)/2) % aes128_blk_len
 
     aes = AES.new(key.decode('hex'), AES.MODE_ECB)
 
-    # Authentication: AES-CBC(K, N, M)
+    # Authentication: AES-CBC-MAC(K, N, M)
     # Block B0
 
-##  From RFC3610:
-##  L: #octest in length of message field
-##   The first block B_0 is formatted as follows, where l(m) is encoded in
-##   most-significant-byte first order:
+##  L: #octets in length of message field
+##   The first block B_0 is formatted as follows, where the length of the message field
+##   is encoded in most-significant-byte-first order:
 ##
 ##      Octet Number   Contents
 ##      ------------   ---------
@@ -82,18 +82,17 @@ def aes_ccm_enc(key, nonce, msg):
         B_i = "{0:0<{width}}".format(msg[(msg_blk_len)*aes128_blk_len*2:len(msg)], width=aes128_blk_len*2)
         xor_out = "{0:0{width}X}".format((long(B_i, 16) ^ long(X, 16)), width=aes128_blk_len*2)
         X = aes.encrypt(xor_out.decode('hex')).encode('hex')
-    # T := X
+    # T := X[:tag_len*2]
     # Authentication tag T is the same length as AES block here so no truncation needed
     # Final tag is encrypted and is calculated after the first encrypted block is calculated
 
     # Encryption: AES-CTR(K, N, M)
 ##    key stream blocks:
-##
 ##      S_i := E( K, A_i )   for i=0, 1, 2, ...
-    # Ciphertext:
-    #   C_i := B_i XOR S_i   for i=1,2,...
-    # Tag:
-    #   U   := T XOR S_0     truncated to tag length
+##    Ciphertext:
+##      C_i := B_i XOR S_i   for i=1,2,...
+##    Tag:
+##      U   := T XOR S_0     truncated to tag length
 ##
 ##   The values A_i are formatted as follows, where the Counter field i is
 ##   encoded in most-significant-byte first order:
@@ -135,6 +134,80 @@ def aes_ccm_enc(key, nonce, msg):
 
     return C+U
 
+def aes_ccm_dec(key, nonce, ctxt):
+    aes128_blk_len = 128/8
+    key_len = 128/8
+    tag_len = 16
+    nonce_len = 12
+    assert len(nonce) <= nonce_len*2, "nonce must be of length less than or equal to 12 octets"
+    assert len(key) == key_len*2, "key must be of length 16 octets"
+    ciphertxt_len = len(ctxt)/2 - tag_len
+    ciphertxt_len_len = 15 - nonce_len
+
+    U = ctxt[ciphertxt_len*2:len(ctxt)]
+    U = "{0:0<{width}}".format(U, width=aes128_blk_len*2)   # in case tag_len was != aes128_blk_len
+    ciphertxt = ctxt[:ciphertxt_len*2]
+    ciphertxt_blk_len = (len(ciphertxt)/2) / aes128_blk_len   # not counting the last non-full block, if any
+    last_bytes        = (len(ciphertxt)/2) % aes128_blk_len
+
+    aes = AES.new(key.decode('hex'), AES.MODE_ECB)
+
+    # Decryption: AES-CTR(K, N, C)
+    # Same key stream as Encryption, XORed with ciphertext
+    A_0_0 ="{0:02X}".format(ciphertxt_len_len - 1)
+    nonce_padded = "{0:0>{width}}".format(nonce, width=nonce_len*2)
+    counter = "{0:0{width}X}".format(0, width=ciphertxt_len_len*2)
+    A_0 = A_0_0 + nonce_padded + counter
+
+    # Decrypting authentication tag:
+    # First block of key stream is XORed with the encrypted tag, U, the last blob of the ciphertext
+    S_0 = aes.encrypt(A_0.decode('hex')).encode('hex')
+    T = "{0:0{width}X}".format((long(U, 16) ^ long(S_0, 16)), width=aes128_blk_len*2)
+    T = T[:tag_len*2]
+
+    # Decrypting ciphertext
+    P = ""  # P: plaintext that should be the same as B1, B2, ...
+    for i in range(1, ciphertxt_blk_len+1):
+        counter = "{0:0{width}X}".format(i, width=ciphertxt_len_len*2)
+        A_i = A_0_0 + nonce_padded + counter
+        S_i = aes.encrypt(A_i.decode('hex')).encode('hex')
+        C_i = ciphertxt[(i-1)*aes128_blk_len*2:i*aes128_blk_len*2]
+        P_i = "{0:0{width}X}".format((long(C_i, 16) ^ long(S_i, 16)), width=aes128_blk_len*2)
+        P  += P_i
+    # handling the last block
+    if (last_bytes):
+        counter = "{0:0{width}X}".format(ciphertxt_blk_len+1, width=ciphertxt_len_len*2)
+        A_i = A_0_0 + nonce_padded + counter
+        S_i = aes.encrypt(A_i.decode('hex')).encode('hex')
+        C_i = "{0:0<{width}}".format(ciphertxt[(ciphertxt_blk_len)*aes128_blk_len*2:len(ciphertxt)], width=aes128_blk_len*2)
+        P_i = "{0:0{width}X}".format((long(C_i, 16) ^ long(S_i, 16)), width=aes128_blk_len*2)
+        P_i = P_i[:last_bytes*2]
+        P  += P_i
+
+    # Authentication: AES-CBC-MAC(K, N, C)
+    # Computed in the same way as in Encryption above with P as input (here B_i = P_i)
+    B_0_0 ="{0:02X}".format( (((tag_len - 2)/2) << 3) | (ciphertxt_len_len - 1) )
+    ciphertxt_len = "{0:0{width}X}".format(len(ciphertxt)/2, width=ciphertxt_len_len*2)
+    B_0 = B_0_0 + nonce_padded + ciphertxt_len
+
+    X = aes.encrypt(B_0.decode('hex')).encode('hex')
+    for i in range(1, ciphertxt_blk_len+1):
+        B_i = P[(i-1)*aes128_blk_len*2:i*aes128_blk_len*2]
+        xor_out = "{0:0{width}X}".format((long(B_i, 16) ^ long(X, 16)), width=aes128_blk_len*2)
+        X = aes.encrypt(xor_out.decode('hex')).encode('hex')
+
+    # handling the last block
+    if (last_bytes):
+        B_i = "{0:0<{width}}".format(P[(ciphertxt_blk_len)*aes128_blk_len*2:len(ciphertxt)], width=aes128_blk_len*2)
+        xor_out = "{0:0{width}X}".format((long(B_i, 16) ^ long(X, 16)), width=aes128_blk_len*2)
+        X = aes.encrypt(xor_out.decode('hex')).encode('hex')
+
+    if (T.upper() != X[:tag_len*2].upper()):
+        return "-1"
+    
+    return P
+
+
 key = "E58D5C8F8C9ED9785679E08ABC7C8116"
 nonce = "A9F593C09EAEEA8BF0C1CF6A"
 pt1 = "0653B5714D1357F4995BDDACBE10873951A1EBA663718D1AF35D2F0D52C79DE49BE622C4A6D90647BA2B004C3E8AE422FD27063AFA19AD883DCCBD97D98B8B0461B5671E75F19701C24042B8D3AF79B9FF62BC448EF9440B1EA3F7E5C0F4BFEFE3E326E62D5EE4CB4B4CFFF30AD5F49A7981ABF71617245B96E522E1ADD78A"
@@ -144,8 +217,21 @@ pt3 = "D1AA8BBC04DFC92FFE2CB7748E70B02F5A91DA14781223A712D44C4BA14A1C78EB02387FE
 C_T = aes_ccm_enc(key, nonce, pt1)
 print("C_T1 = " + C_T)
 
+P_T = aes_ccm_dec(key, nonce, C_T)
+print("P_T1 = " + P_T)
+assert P_T == pt1, "decrypted ciphertext differs from plaintext"
+
 C_T = aes_ccm_enc(key, nonce, pt2)
 print("C_T2 = " + C_T)
 
+P_T = aes_ccm_dec(key, nonce, C_T)
+print("P_T2 = " + P_T)
+assert P_T == pt2, "decrypted ciphertext differs from plaintext"
+
 C_T = aes_ccm_enc(key, nonce, pt3)
 print("C_T3 = " + C_T)
+
+P_T = aes_ccm_dec(key, nonce, C_T)
+print("P_T3 = " + P_T)
+assert P_T == pt3, "decrypted ciphertext differs from plaintext"
+
