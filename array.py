52b8abe1434f22ae0bbb4404cbb7bfe3fb1377f4
Mark Grimes <mark@stateful.net>
2015-10-19 16:31:43 -0700

    PEP8

--- array.py
+++ array.py
@@ -1,17 +1,16 @@
-
 from radix import *
 
-#------------------------------------------------------------------------
+
 #
 # Output the definition of an array from a list:
 #
-def cArrayDefFromList(arraytype, arrayname, arraylist, arrayradix=2**32):
+def cArrayDefFromList(arraytype, arrayname, arraylist, arrayradix=2 ** 32):
     if arrayname != "":
         arrayelems = "[" + str(len(arraylist)) + "] = "
         print(arraytype, arrayname + arrayelems)
 
     print("{ ", end="")
-    for i in range(0,len(arraylist)-1):
+    for i in range(0, len(arraylist) - 1):
         if i and i % 16 == 0:
             print()
             print("  ", end="")
@@ -22,15 +21,14 @@ def cArrayDefFromList(arraytype, arrayname, arraylist, arrayradix=2**32):
         print("  ", end="")
     print(Hex(arraylist[i], arrayradix) + " }", end="")
 
+
 #
 # Output the definition of an array (from a value):
 #
-def cArrayDef(arraytype, arrayname, arrayvalue, arraylen=0, arrayradix=2**32, littleendian=True):
+def cArrayDef(arraytype, arrayname, arrayvalue, arraylen=0, arrayradix=2 ** 32, littleendian=True):
     if littleendian:
         arraylist = int2lelist(arrayvalue, arrayradix, arraylen)
     else:
         arraylist = int2belist(arrayvalue, arrayradix, arraylen)
 
     cArrayDefFromList(arraytype, arrayname, arraylist, arrayradix)
-
-
