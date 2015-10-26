ec08d57224af74ac41e802b1b8327fbd4ba4630b
Mark Grimes <mark.e.grimes@leidos.com>
2015-10-26 14:38:57 -0700

    Add test stub

---
+++ bfkeyexp_tests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class BFKeyExpansionTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
